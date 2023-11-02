b7cc9ffeae5bb2d87a552580e10027fe793589d0
Jhordan S <jhordan.kendy@outlook.com>
2023-11-01 22:26:49 -0300

    Feat: capturando método.

--- public/index.php
+++ public/index.php
@@ -1,11 +1,17 @@
 <?php 
 
-require_once './vendor/autoload.php';
+// require_once './vendor/autoload.php';
 
 if ($_SERVER['REQUEST_URI']) {
     $url = explode('/', ltrim($_SERVER['REQUEST_URI'], '/'));
 
     if ($url[0] === 'api') {
-        echo 'api';
+        array_shift($url);
+        $service = $url[0];
+
+        array_shift($url);
+
+        $method = $_SERVER['REQUEST_METHOD'];
+        
     }
 }
